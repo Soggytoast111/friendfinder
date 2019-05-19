e6ae7bf1406876b082c3377cf922cfd19d3740d1
soggytoast111 <joncurto@gmail.com>
2019-05-18 23:54:54 -0400

    work in progress

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
+var friendData = require("../data/friends")
+
 module.exports = function(app) {
-    app.get("/api/tables", function(req, res) {
-      res.json(tableData)
+    app.get("/api/friends", function(req, res) {
+      res.json(friendData)
+      console.log("he hit the friends api!")
     })
 
-    app.post("/api/tables", function(req, res) {
-      if (tableData.length < 5) {
-        tableData.push(req.body)
-        res.json(true)
-      }
-      else {
-        waitListData.push(req.body)
-        res.json(false)
-      }
+    app.post("/api/friends", function(req, res) {
+      friendData.push(req.body)
+      res.json("okay!")
+      console.log(friendData)
+      console.log("here is the req")
+      console.log(req)
     })
 }
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var path = require("path");
 var app = express();
 var PORT = 3031
 
-require("./app/routing/htmlRoutes")(app);
-require("./routes/apiRoutes")(app);
 
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
